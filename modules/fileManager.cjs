d325bfe8f1f8c8dc44da090ac28010ecc6110f9a
Omer <omersabribalci@gmail.com>
2026-07-12 16:13:44 -0400

    feat: Implement film statistics and enhance server response handling

--- modules/fileManager.cjs
+++ modules/fileManager.cjs
@@ -13,4 +13,80 @@ const getAllFilms = async () => {
   }
 };
 
-module.exports = { getAllFilms };
+const getFilmIstatistics = (films) => {
+  const totalfilmCount = films.length;
+  let watchedCount = 0;
+  let notWatchedCount = 0;
+  let totalRating = 0;
+
+  for (let film of films) {
+    totalRating += film.rating;
+    if (film.watched) {
+      watchedCount++;
+    } else {
+      notWatchedCount++;
+    }
+  }
+
+  let averageRating = (totalRating / totalfilmCount).toFixed(1);
+
+  let lastThreeAdded = films.slice(totalfilmCount - 3);
+
+  let lastAddedList = `
+        <ul>
+          ${lastThreeAdded.map((film) => `<li>${film.title} (${film.year})</li>`).join("")}
+        </ul>
+        `;
+
+  const categories = films.map((film) => film.category);
+  let categoryBased = {};
+  for (let cat of categories) {
+    categoryBased[cat] = (categoryBased[cat] || 0) + 1;
+  }
+
+  return {
+    totalfilmCount,
+    watchedCount,
+    notWatchedCount,
+    averageRating,
+    lastAddedList,
+    categoryBased,
+  };
+};
+
+const renderFilmsList = (filmsToRender) => {
+  const content = filmsToRender
+    .map(
+      (film) => `
+        <div class="film">
+            <img />
+            <div>${film.title}</div>
+            <div>${film.year}</div>
+            <div>${film.rating}</div>
+            <a href="/films/${film.id}"> Film detayına git ➡️</a>
+        </div>
+    `,
+    )
+    .join("");
+
+  return { content };
+};
+
+const renderCategoryLinks = (films) => {
+  const categoriesSet = new Set(films.map((f) => f.category));
+  const categories = Array.from(categoriesSet);
+
+  const categoryLinks = `
+        <a href="/films"><li>Hepsini Göster</li></a>
+        ${categories.map((cat) => `<a href="/films/category/${cat}"><li>${cat}</li></a>`).join("")}
+    `;
+
+  return { categoryLinks };
+};
+
+module.exports = {
+  getAllFilms,
+  getFilmIstatistics,
+  renderFilmsList,
+  renderCategoryLinks,
+};
--- server.js
+++ server.js
@@ -2,9 +2,16 @@
 
 const http = require("node:http");
 const path = require("node:path");
-const fs = require("node:fs");
+const fs = require("node:fs/promises");
+const fss = require("node:fs"); // stream için
+
 const eventBus = require("./modules/eventBus.cjs");
-const { getAllFilms } = require("./modules/fileManager.cjs");
+const {
+  getAllFilms,
+  getFilmIstatistics,
+  renderFilmsList,
+  renderCategoryLinks,
+} = require("./modules/fileManager.cjs");
 const { logMessage } = require("./modules/logger.mjs");
 
 eventBus.on("filmViewed", (title) => {
@@ -16,203 +23,159 @@ eventBus.on("reportGenerated", (fileName) => {
 });
 
 const server = http.createServer(async (req, res) => {
-  logMessage("INFO", `${req.method} ${req.url}`);
+  try {
+    const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+    const pathName = parsedUrl.pathname;
 
-  // get all films
-  const data = await getAllFilms();
-  const films = data.films;
+    logMessage("INFO", `${req.method} ${req.url}`);
 
-  // calculate statistics
-  const totalfilmCount = films.length;
-  let watchedCount = 0;
-  let notWatchedCount = 0;
-  let totalRating = 0;
+    if (pathName === "/") {
+      const data = await getAllFilms();
+      const films = data.films;
 
-  for (let film of films) {
-    totalRating += film.rating;
-    if (film.watched) {
-      watchedCount++;
-    } else {
-      notWatchedCount++;
-    }
-  }
+      const {
+        totalfilmCount,
+        watchedCount,
+        notWatchedCount,
+        averageRating,
+        lastAddedList,
+      } = getFilmIstatistics(films);
 
-  let averageRating = (totalRating / totalfilmCount).toFixed(1);
-
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-
-    const homePagePath = path.join(__dirname, "templates", "home.html");
-    const title = "Film Arşivi Yönetimi";
-
-    let lastThreeAdded = films.slice(totalfilmCount - 3);
-
-    let lastAddedList = `
-    <ul>
-      ${lastThreeAdded.map((film) => `<li>${film.title} (${film.year})</li>`).join("")}
-    </ul>
-    `;
-
-    let html = fs.readFileSync(homePagePath, "utf-8");
-
-    html = html.replace("{{title}}", title);
-    html = html.replace("{{totalfilmCount}}", totalfilmCount);
-    html = html.replace("{{watchedCount}}", watchedCount);
-    html = html.replace("{{notWatchedCount}}", notWatchedCount);
-    html = html.replace("{{averageRating}}", averageRating);
-    html = html.replace("{{lastAddedList}}", lastAddedList);
-
-    res.end(html);
-  } else if (req.url === "/films") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-
-    const filmsPagePath = path.join(__dirname, "templates", "films.html");
-    const title = "Filmler";
-    const categoriesSet = new Set(films.map((film) => film.category));
-    const categories = Array.from(categoriesSet);
-
-    const categoryLinks = `
-        <a href="/films"><li>Hepsini Göster</li></a>
-        ${categories
-          .map((cat) => {
-            return `<a href="/films/category/${cat}"><li>${cat}</li></a>`;
-          })
-          .join("")}
-    `;
-    const content = `
-        ${films
-          .map((film) => {
-            return `
-          <div  class="film">
-            <img />
-            <div>${film.title}</div>
-            <div>${film.year}</div>
-            <div>${film.rating}</div>
-            <a href="/films/${film.id}"> Film detayına git ➡️</a>
-          </div>
-          `;
-          })
-          .join("")}
-    `;
-
-    let html = fs.readFileSync(filmsPagePath, "utf-8");
-
-    html = html.replace("{{title}}", title);
-    html = html.replace("{{categoryLinks}}", categoryLinks);
-    html = html.replace("{{content}}", content);
-
-    res.end(html);
-  } else if (req.url.startsWith("/films/category")) {
-    const category = req.url.split("/")[3];
-    const categorizedFilms = films.filter((film) => film.category === category);
-
-    res.writeHead(200, { "Content-Type": "text/html" });
-
-    const filmsPagePath = path.join(__dirname, "templates", "films.html");
-    const title = "Filmler";
-    const categoriesSet = new Set(films.map((film) => film.category));
-    const categories = Array.from(categoriesSet);
-
-    const categoryLinks = `
-        <a href="/films"><li>Hepsini Göster</li></a>
-        ${categories
-          .map((cat) => {
-            return `<a href="/films/category/${cat}"><li>${cat}</li></a>`;
-          })
-          .join("")}
-    `;
-
-    const content = `
-        ${categorizedFilms
-          .map((film) => {
-            return `
-          <div  class="film">
-            <img />
-            <div>${film.title}</div>
-            <div>${film.year}</div>
-            <div>${film.rating}</div>
-            <a href="/films/${film.id}"> Film detayına git ➡️</a>
-          </div>
-          `;
-          })
-          .join("")}
-    `;
-
-    let html = fs.readFileSync(filmsPagePath, "utf-8");
-
-    html = html.replace("{{title}}", title);
-    html = html.replace("{{categoryLinks}}", categoryLinks);
-    html = html.replace("{{content}}", content);
-
-    res.end(html);
-  } else if (req.url.startsWith("/films/")) {
-    const id = req.url.split("/")[2];
-    const film = films.find((film) => film.id.toString() === id);
-
-    if (film) {
       res.writeHead(200, { "Content-Type": "text/html" });
 
-      eventBus.emit("filmViewed", film.title);
-      let title = film.title;
-      let content = `
-            <img />
-            <div>${film.title}</div>
-            <div>${film.director}</div>
-            <div>${film.year}</div>
-            <div>${film.rating}</div>
-            <div>${film.category}</div>
-            <div>${film.watched ? "İzlendi" : "İzlenmedi"}</div>
-      `;
-      const filmDetailsPath = path.join(
-        __dirname,
-        "templates",
-        "film-detail.html",
-      );
-      let html = fs.readFileSync(filmDetailsPath, "utf-8");
+      const homePagePath = path.join(__dirname, "templates", "home.html");
+      const title = "Film Arşivi Yönetimi";
+
+      let html = await fs.readFile(homePagePath, "utf-8");
+
       html = html.replace("{{title}}", title);
+      html = html.replace("{{totalfilmCount}}", totalfilmCount);
+      html = html.replace("{{watchedCount}}", watchedCount);
+      html = html.replace("{{notWatchedCount}}", notWatchedCount);
+      html = html.replace("{{averageRating}}", averageRating);
+      html = html.replace("{{lastAddedList}}", lastAddedList);
+
+      res.end(html);
+    } else if (pathName === "/films") {
+      const data = await getAllFilms();
+      const films = data.films;
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+
+      const filmsPagePath = path.join(__dirname, "templates", "films.html");
+      const title = "Filmler";
+
+      const { categoryLinks } = renderCategoryLinks(films);
+      const { content } = renderFilmsList(films);
+
+      let html = await fs.readFile(filmsPagePath, "utf-8");
+
+      html = html.replace("{{title}}", title);
+      html = html.replace("{{categoryLinks}}", categoryLinks);
       html = html.replace("{{content}}", content);
-      res.end(html);
-    } else {
-      res.writeHead(404, { "Content-Type": "text/html" });
-      const notFoundPagePath = path.join(__dirname, "templates", "404.html");
-      logMessage("ERROR", "Film bulunamadı...");
-      let html = fs.readFileSync(notFoundPagePath, "utf-8");
-      res.end(html);
-    }
-  } else if (req.url === "/api/films") {
-    const filmsJsonPath = path.join(__dirname, "data", "films.json");
-    let json = fs.readFileSync(filmsJsonPath, "utf-8");
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(json);
-  } else if (req.url === "/api/stats") {
-    const categories = films.map((film) => film.category);
-    let lookup = {};
-    for (let cat of categories) {
-      lookup[cat] = (lookup[cat] || 0) + 1;
-    }
 
-    let statistics = {
-      stats: {
-        totalFilms: totalfilmCount,
-        watchedFilms: watchedCount,
-        notWatchedFilmes: notWatchedCount,
-        averageRating: averageRating,
-        categories: lookup,
-      },
-    };
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(statistics));
-  } else if (req.url === "/reports/export") {
-    res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(html);
+    } else if (pathName.startsWith("/films/category")) {
+      const data = await getAllFilms();
+      const films = data.films;
 
-    try {
+      const category = pathName.split("/")[3];
+      const categorizedFilms = films.filter(
+        (film) => film.category === category,
+      );
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+
+      const filmsPagePath = path.join(__dirname, "templates", "films.html");
+      const title = "Filmler";
+
+      const { categoryLinks } = renderCategoryLinks(films);
+      const { content } = renderFilmsList(categorizedFilms);
+
+      let html = await fs.readFile(filmsPagePath, "utf-8");
+
+      html = html.replace("{{title}}", title);
+      html = html.replace("{{categoryLinks}}", categoryLinks);
+      html = html.replace("{{content}}", content);
+
+      res.end(html);
+    } else if (pathName.startsWith("/films/")) {
+      const data = await getAllFilms();
+      const films = data.films;
+
+      const id = pathName.split("/")[2];
+      const film = films.find((film) => film.id.toString() === id);
+
+      if (film) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+
+        eventBus.emit("filmViewed", film.title);
+        let title = film.title;
+        let content = `
+              <img />
+              <div>${film.title}</div>
+              <div>${film.director}</div>
+              <div>${film.year}</div>
+              <div>${film.rating}</div>
+              <div>${film.category}</div>
+              <div>${film.watched ? "İzlendi" : "İzlenmedi"}</div>
+        `;
+        const filmDetailsPath = path.join(
+          __dirname,
+          "templates",
+          "film-detail.html",
+        );
+        let html = await fs.readFile(filmDetailsPath, "utf-8");
+        html = html.replace("{{title}}", title);
+        html = html.replace("{{content}}", content);
+        res.end(html);
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        const notFoundPagePath = path.join(__dirname, "templates", "404.html");
+        logMessage("ERROR", "Film bulunamadı...");
+        let html = await fs.readFile(notFoundPagePath, "utf-8");
+        res.end(html);
+      }
+    } else if (pathName === "/api/films") {
+      const filmsJsonPath = path.join(__dirname, "data", "films.json");
+      let json = await fs.readFile(filmsJsonPath, "utf-8");
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(json);
+    } else if (pathName === "/api/stats") {
+      const data = await getAllFilms();
+      const films = data.films;
+      const {
+        totalfilmCount,
+        watchedCount,
+        notWatchedCount,
+        averageRating,
+        categoryBased,
+      } = getFilmIstatistics(films);
+
+      let statistics = {
+        stats: {
+          totalFilms: totalfilmCount,
+          watchedFilms: watchedCount,
+          notWatchedFilmes: notWatchedCount,
+          averageRating: averageRating,
+          categories: categoryBased,
+        },
+      };
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(statistics));
+    } else if (pathName === "/reports/export") {
+      const data = await getAllFilms();
+      const films = data.films;
+
+      res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+
       const reportPath = path.join(__dirname, "reports", "films-export.txt");
-      const writeStream = fs.createWriteStream(reportPath);
+      const writeStream = fss.createWriteStream(reportPath);
 
       writeStream.on("error", (err) => {
         logMessage("ERROR", "Rapor yazılırken hata oluştu");
         console.error("Rapor yazılırken hata oluştu:", err);
-        res.end("<p>Report creation failed.</p>");
+        if (!res.headersSent) res.end("<p>Rapor oluşturulamadı.</p>");
       });
 
       writeStream.write("=== Film Arşivi Raporu ===\n");
@@ -233,19 +196,23 @@ const server = http.createServer(async (req, res) => {
 
       writeStream.end(() => {
         eventBus.emit("reportGenerated", "films-export.txt");
-        res.end("<p>Report is created successfully in reports folder.</p>");
+        res.end("<p>Rapor reports klasörüne başarıyla oluşturuldu.</p>");
       });
-    } catch (err) {
-      logMessage("ERROR", "Beklenmedik hata oluştu");
-      console.error("Beklenmedik hata:", err);
-      res.end("<p>Unexpected error occurred.</p>");
+    } else {
+      res.writeHead(404, { "Content-Type": "text/html; charset=utf-8" });
+      const notFoundPagePath = path.join(__dirname, "templates", "404.html");
+      logMessage("ERROR", "Sayfa bulunamadı...");
+      let html = await fs.readFile(notFoundPagePath, "utf-8");
+      res.end(html);
     }
-  } else {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    const notFoundPagePath = path.join(__dirname, "templates", "404.html");
-    logMessage("ERROR", "Sayfa bulunamadı...");
-    let html = fs.readFileSync(notFoundPagePath, "utf-8");
-    res.end(html);
+  } catch (error) {
+    logMessage("ERROR", `Sunucu Hatası: ${error.message}`);
+    console.error("Kritik Sunucu Hatası:", error);
+
+    if (!res.headersSent) {
+      res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end("500 - Sunucu İçi Hata. Lütfen daha sonra tekrar deneyin.");
+    }
   }
 });
 
